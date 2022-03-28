812dcd1e2d31d84cbdb334d45978429bc02f4de4
userNameMertcan <102524479+userNameMertcan@users.noreply.github.com>
2022-03-28 15:02:16 +0300

    Add files via upload

---
+++ react-props-practice-forked/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+//4. Import the contacts.js file to create card components.
